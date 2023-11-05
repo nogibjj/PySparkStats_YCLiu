72eebb9f671a15fe72ed106c15fdbb8dbb128a64
Yun-Chung LIU <46064664+halfmoonliu@users.noreply.github.com>
2023-11-05 20:52:22 +0000

    reformat main

--- main.py
+++ main.py
@@ -2,28 +2,28 @@ from pyspark.sql import SparkSession
 
 
 def QueryProdRev():
-     # create spark session
-     spark = SparkSession.builder.appName("PySpark")\
-         .config("spark.some.config.option", "some-value")\
-         .getOrCreate()
-     # load dataset
-     Dataset = spark.read.csv("SalesTransactionV4a.csv", header=True, inferSchema=True)
-     # add new column
-     Dataset = Dataset.withColumn('Revenue', Dataset.Price*Dataset.Quantity)
-     # create new dataframe
-     ProductRevenue = Dataset.groupBy("ProductNo","ProductName")\
-         .sum("Revenue")\
-         .withColumnRenamed("sum(Revenue)", "SumRevenue")
-     # create view for sql query
-     ProductRevenue.createOrReplaceTempView("ProductRevenue")
+    # create spark session
+    spark = SparkSession.builder.appName("PySpark")\
+                        .config("spark.some.config.option", "some-value")\
+                        .getOrCreate()
+    # load dataset
+    Dataset = spark.read.csv("SalesTransactionV4a.csv", header=True, inferSchema=True)
+    # add new column
+    Dataset = Dataset.withColumn('Revenue', Dataset.Price*Dataset.Quantity)
+    # create new dataframe
+    ProductRevenue = Dataset.groupBy("ProductNo","ProductName")\
+                            .sum("Revenue")\
+                            .withColumnRenamed("sum(Revenue)", "SumRevenue")
+    # create view for sql query
+    ProductRevenue.createOrReplaceTempView("ProductRevenue")
      
-     # display query result
-     results_view = spark.sql("SELECT * FROM ProductRevenue WHERE ProductNo = '22491'")
-     results_view.show()
+    # display query result
+    results_view = spark.sql("SELECT * FROM ProductRevenue WHERE ProductNo = '22491'")
+    results_view.show()
      
-     # output revenue of a product
-     RevenueOutput = results_view.collect()[0][2]
-     return RevenueOutput
+    # output revenue of a product
+    RevenueOutput = results_view.collect()[0][2]
+    return RevenueOutput
 
 if __name__ == "__main__":
      QueryProdRev()
